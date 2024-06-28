77c2c81841608988da3ac29dab6723e6663d905d
saeed-cefalo <saeed.anwar@cefalo.com>
2024-06-28 12:38:36 +0600

    init: Read query and repeat

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    // welcome message
+    println!("Hi, I'm a Rust chatbot! How can I help you today?");
+
+    loop {
+        let mut query = String::new();
+        io::stdin()
+            .read_line(&mut query)
+            .expect("Failed to read line");
+
+        println!("You said: {}", query.trim());
+    }
 }
